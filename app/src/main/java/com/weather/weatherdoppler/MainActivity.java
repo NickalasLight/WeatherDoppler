85a5c4111a136ae7f54f2ceae6e79a856fd9f146
NickalasLight <nickalas.93@gmail.com>
2019-06-29 17:44:53 -0400

    working static map and overlaid doppler.

--- app/src/main/java/com/weather/weatherdoppler/MainActivity.java
+++ app/src/main/java/com/weather/weatherdoppler/MainActivity.java
@@ -8,6 +8,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
 
+import com.mapbox.api.staticmap.v1.MapboxStaticMap;
+import com.mapbox.api.staticmap.v1.StaticMapCriteria;
+import com.mapbox.geojson.Point;
 import com.mapbox.mapboxsdk.Mapbox;
 import com.mapbox.mapboxsdk.camera.CameraPosition;
 import com.mapbox.mapboxsdk.geometry.LatLng;
@@ -15,6 +18,10 @@ import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 import com.mapbox.mapboxsdk.maps.Style;
+import com.mapbox.api.staticmap.*;
+
+import java.time.Instant;
+
 
 public class MainActivity extends AppCompatActivity {
     private MapView mapView;
@@ -22,25 +29,55 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+
+final double latitude =  27.6648;
+final double longitude = -81.5158;
+long timeStamp = Instant.now().getEpochSecond();
+timeStamp = timeStamp - (timeStamp % 600);
+int zoom = 2;
+
+
+        MapboxStaticMap staticImage = MapboxStaticMap.builder()
+                .accessToken("pk.eyJ1Ijoibmlja2FsYXMiLCJhIjoiY2p2eTJkOXI2MGJmdDQ4cGMxMHllZzQxYSJ9.MGT4wEduz99RGkCIwKTbCw")
+                .styleId(StaticMapCriteria.LIGHT_STYLE)
+                .cameraPoint(Point.fromLngLat(longitude, latitude)) // Image's centerpoint on map
+                .cameraZoom(zoom)
+                .width(512) // Image width
+                .height(512) // Image height
+                .retina(true) // Retina 2x image will be returned
+                .build();
+
+
+
         Mapbox.getInstance(this, "pk.eyJ1Ijoibmlja2FsYXMiLCJhIjoiY2p2eTJkOXI2MGJmdDQ4cGMxMHllZzQxYSJ9.MGT4wEduz99RGkCIwKTbCw");
         setContentView(R.layout.activity_main);
 
 
-        ImageView myImageView = findViewById(R.id.dopplerimageView);
+        ImageView myImageView = findViewById(R.id.staticmapimageView);
         UrlBuilder myUrlBuilder = new UrlBuilder();
         //https://tilecache.rainviewer.com/v2/radar/1561737000/1024/2/39.290386/-76.612190/2/1_1.png
-        String Url = myUrlBuilder.buildUrl("1561737000","1024","2","51.50550","-0.07520","2","1_1");
-
-        myImageView.setTag(Url);
 
 
-
+        myImageView.setTag(staticImage.url().toString());
         ReturnDopplerImg myDop = new ReturnDopplerImg();
-
         myDop.execute(myImageView);
         //myDop.onPostExectute();
        //myImageView.setImageDrawable(myDop.LoadImageFromWebOperations(""));
 
+try {
+    ImageView myDopImageView = findViewById(R.id.dopplerimageView);
+    UrlBuilder myDopUrlBuilder = new UrlBuilder();
+    //https://tilecache.rainviewer.com/v2/radar/1561737000/1024/2/39.290386/-76.612190/2/1_1.png
+    String DopUrl = myUrlBuilder.buildUrl(Long.toString(timeStamp),"512",Integer.toString(zoom),Double.toString(latitude),Double.toString(longitude),"2","1_1");
+    myDopImageView.setTag(DopUrl);
+    ReturnDopplerImg myDopImg = new ReturnDopplerImg();
+    myDopImg.execute(myDopImageView);
+}
+catch(Exception ex) {
+    ex.printStackTrace();
+}
 
 
 
@@ -52,7 +89,7 @@ public class MainActivity extends AppCompatActivity {
 
 
                 CameraPosition position = new CameraPosition.Builder()
-                        .target(new LatLng(51.50550, -0.07520))
+                        .target(new LatLng(latitude, longitude))
                         .zoom(2)
                         .tilt(0)
                         .build();
